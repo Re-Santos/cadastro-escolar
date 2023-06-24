08b1bd60e97751e7ee2c897e9308cb32b67b64b4
Re-Santos <renata.prog@gmail.com>
2023-06-23 22:39:02 -0300

    [template] Criei a estrutura inicial de arquivos

---
+++ cadastro-escolar/src/Components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import List from './List/List'
+import Data from './Data/Data'
+
+const Home = () => {
+  return (
+    <div>
+      <List/>
+      <Data/>
+    </div>
+  )
+}
+
+export default Home
